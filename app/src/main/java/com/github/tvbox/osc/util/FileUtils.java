1eef7a4ffacd6f87d576a3451524aaf700143248
hushujiang <260068900@qq.com>
2025-10-03 00:29:53 +0800

    1

--- app/src/main/java/com/github/tvbox/osc/util/FileUtils.java
+++ app/src/main/java/com/github/tvbox/osc/util/FileUtils.java
@@ -2,48 +2,104 @@ package com.github.tvbox.osc.util;
 
 import android.os.Environment;
 import android.text.TextUtils;
+
 import com.github.tvbox.osc.base.App;
 
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 
 public class FileUtils {
-    private static Map<String, byte[]> cacheMap = new HashMap<>();
 
-    // 新增方法：读取模块
-    public static String readModule(String moduleName) {
+    public static boolean writeSimple(byte[] data, File dst) {
         try {
-            if (cacheMap.containsKey(moduleName)) {
-                return new String(cacheMap.get(moduleName));
+            if (dst.exists())
+                dst.delete();
+            BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(dst));
+            bos.write(data);
+            bos.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static byte[] readSimple(File src) {
+        try {
+            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(src));
+            int len = bis.available();
+            byte[] data = new byte[len];
+            bis.read(data);
+            bis.close();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void copyFile(File source, File dest) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(source);
+            os = new FileOutputStream(dest);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) {
+                os.write(buffer, 0, length);
             }
-            return getAssetFile(moduleName);
-        } catch (Exception e) {
-            return "";
+        } finally {
+            is.close();
+            os.close();
         }
     }
 
-    // 新增方法：写入缓存
-    public static void writeCache(String key, byte[] data) {
-        cacheMap.put(key, data);
+    public static void recursiveDelete(File file) {
+        if (!file.exists())
+            return;
+        if (file.isDirectory()) {
+            for (File f : file.listFiles()) {
+                recursiveDelete(f);
+            }
+        }
+        file.delete();
     }
 
-    // 新增方法：读取缓存字节
-    public static byte[] readCache(String key) {
-        return cacheMap.getOrDefault(key, null);
+    public static String readFileToString(String path, String charsetName) {
+        // 定义返回结果
+        String jsonString = "";
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(path)), charsetName));// 读取文件
+            String thisLine = null;
+            while ((thisLine = in.readLine()) != null) {
+                jsonString += thisLine;
+            }
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException el) {
+                }
+            }
+        }
+        // 返回拼接好的JSON String
+        return jsonString;
     }
 
-    // ... [保持原有方法不变] ...
-    
-    public static String getAssetFile(String assetName) throws IOException {
-        InputStream is = App.getInstance().getAssets().open(assetName);
-        byte[] data = new byte[is.available()];
-        is.read(data);
-        return new String(data, "UTF-8");
-    }
-
-    // ... [保持其余代码不变] ...
-}
     public static String getAssetFile(String assetName) throws IOException {
         InputStream is = App.getInstance().getAssets().open(assetName);
         byte[] data = new byte[is.available()];
